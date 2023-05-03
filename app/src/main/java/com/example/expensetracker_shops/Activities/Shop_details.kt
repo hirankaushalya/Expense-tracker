7cb74d3d3c26090932454add9efee8a55b04f5ff
hirankaushalya <99481438+hirankaushalya@users.noreply.github.com>
2023-05-03 20:26:04 +0530

    Implement update function for update shop details

--- app/src/main/java/com/example/expensetracker_shops/Activities/Shop_details.kt
+++ app/src/main/java/com/example/expensetracker_shops/Activities/Shop_details.kt
@@ -2,8 +2,14 @@ package com.example.expensetracker_shops.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.example.expensetracker_shops.Models.ShopModel
 import com.example.expensetracker_shops.R
+import com.google.firebase.database.FirebaseDatabase
 
 class Shop_details : AppCompatActivity() {
 
@@ -22,6 +28,70 @@ class Shop_details : AppCompatActivity() {
         //tvShopId = findViewById(R.id.tvShopId)
         initView()
         setValuesToviews()
+
+        btnUpdate.setOnClickListener{
+            openUpdateDialog(
+                intent.getStringExtra("shopId").toString(),
+                intent.getStringExtra("shopName").toString(),
+                intent.getStringExtra("shopAddress").toString(),
+                intent.getStringExtra("shopMobNo").toString()
+            )
+        }
+    }
+    private fun openUpdateDialog(
+        shopId:String,
+        shopName:String,
+        shopAddress:String,
+        shopMobNo:String
+    ){
+        val mDialog = AlertDialog.Builder(this)
+        val inflater=layoutInflater
+        val mDialogView = inflater.inflate(R.layout.activity_update_dialog,null)
+
+        mDialog.setView(mDialogView)
+
+        val upShopname = mDialogView.findViewById<EditText>(R.id.upShopName)
+        val upShopAddress = mDialogView.findViewById<EditText>(R.id.upShopAddress)
+        val upShopMob = mDialogView.findViewById<EditText>(R.id.upShopMob)
+
+        val btnUpdateData = mDialogView.findViewById<Button>(R.id.btnUpdateData)
+
+        upShopname.setText(intent.getStringExtra("shopName").toString())
+        upShopAddress.setText(intent.getStringExtra("shopAddress").toString())
+        upShopMob.setText(intent.getStringExtra("shopMobNo").toString())
+
+        mDialog.setTitle("Updating $shopName Record")
+
+        val alertDialog = mDialog.create()
+        alertDialog.show()
+
+        btnUpdateData.setOnClickListener {
+            updateShopData(
+                shopId,
+                upShopname.text.toString(),
+                upShopAddress.text.toString(),
+                upShopMob.text.toString()
+            )
+            Toast.makeText(applicationContext, "Shop Data Updated",Toast.LENGTH_LONG).show()
+
+            tvShopName.text = upShopname.text.toString()
+            tvShopAddress.text = upShopAddress.text.toString()
+            tvShopMob.text = upShopMob.text.toString()
+
+            alertDialog.dismiss()
+        }
+
+
+    }
+    private fun updateShopData(
+        shopId:String,
+        shopName: String,
+        shopAddress: String,
+        shopMobNo : String
+    ){
+        val dbRef = FirebaseDatabase.getInstance().getReference("Shops").child(shopId)
+        val shopInfo = ShopModel(shopId, shopName, shopAddress, shopMobNo)
+        dbRef.setValue(shopInfo)
     }
 
     private fun initView(){
---
+++ app/src/main/java/com/example/expensetracker_shops/Activities/UpdateDialog.kt
@@ -0,0 +1,12 @@
+package com.example.expensetracker_shops.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.expensetracker_shops.R
+
+class updateDialog : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_update_dialog)
+    }
+}
