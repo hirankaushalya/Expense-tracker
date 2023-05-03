30bec1e8c47d73f2ce8fc7c07fe5c4e3dcf37d6b
hirankaushalya <99481438+hirankaushalya@users.noreply.github.com>
2023-05-03 09:51:22 +0530

    connected to Firebase database and impleneted main page

--- app/src/main/java/com/example/expensetracker_shops/MainActivity.kt
+++ app/src/main/java/com/example/expensetracker_shops/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.expensetracker_shops
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnInsertData: Button
+    private lateinit var btnFetchData: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        btnInsertData = findViewById(R.id.btnInsertData)
+        btnFetchData = findViewById(R.id.btnFetchData)
+
+
+
     }
 }
