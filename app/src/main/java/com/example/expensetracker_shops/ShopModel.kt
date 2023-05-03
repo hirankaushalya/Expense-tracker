8bf9b9b69bbf50cd2dae0f66109326c2d3947954
hirankaushalya <99481438+hirankaushalya@users.noreply.github.com>
2023-05-03 11:56:20 +0530

    create Shop Model

---
+++ app/src/main/java/com/example/expensetracker_shops/ShopModel.kt
@@ -0,0 +1,8 @@
+package com.example.expensetracker_shops
+
+data class ShopModel(
+    var shopId : String? = null ,
+    var shopName : String? = null ,
+    var shopAddress : String? = null,
+    var shopMobNo : String? = null
+)
